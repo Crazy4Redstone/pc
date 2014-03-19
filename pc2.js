0812aa5dab7bcb4d76f67bc0f9c622891c9074d6
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-18 19:57:17 -0500

    Create pc2.js

---
+++ pc2.js
@@ -0,0 +1,20 @@
+// Pc McPE
+// Made by Crazy4Redstone
+
+// Blocks
+
+Block.defineBlock (23,'Chiseled Stone Brick',[['stonebrick',3]],1,false,0)
+
+// Items
+
+
+
+// Foods
+
+
+
+// Crafting Recipes
+
+ModPE.addCraftRecipe (23,1,0,[1,0,1,0,1,0,1,0])
+
+// Furnace Recipes
